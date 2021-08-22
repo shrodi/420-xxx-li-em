8aad896ec4fd2c28704fb5e1022b5f350eaa5a7f
Eric Martel <e@mar.tel>
2021-08-22 19:32:46 -0400

    Changé commentaire

--- DemarrerNotes.cmd
+++ DemarrerNotes.cmd
@@ -11,7 +11,7 @@ if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && 
 :: Met à jour le dépôt Git (nécessite que «Git for Windows» soit installé: https://gitforwindows.org/)
 git pull
 
-:: Démarre les notes de cours (ayant même nom que dossier courant) avec le fureteur par défaut
+:: Démarre les notes de cours (fichier .html) avec le fureteur par défaut
 ::for %%I in (.) do start %%~nxI.html
 for %%I in (*.html) do start %%~nxI
 
